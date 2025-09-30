053d0bb0a291e6e60ddee1d23b750d3193b9b030
Basem Atiye <basematiye1@gmail.com>
2025-09-30 15:18:08 +0300

    full code without bonus

--- grocery.py
+++ grocery.py
@@ -11,18 +11,23 @@ groceries = {
     "blueberries": 9
 }
 
+n=len(groceries)
+for i in range (n):
+    item=list(groceries.keys())[i]
+    price=groceries[item]
+    print(f"{item}: ${price}")
+
 cart={}
 user_input = ""
 total_price = 0
 while user_input != "done":
-    user_input = input("Enter an item to check its price (or 'done' to exit): ")
+    user_input = input("Enter an item to add to your cart (or 'done' to exit): ")
     if user_input == "done":
         print("Thank you for shopping with us!")
         print("Your cart:", cart)
         print("Total price: $", total_price)
         break
     elif user_input in groceries:
-        print(f"The price of {user_input} is ${groceries[user_input]}.")
         cart[user_input] = groceries[user_input]
         total_price += groceries[user_input]
     else:
