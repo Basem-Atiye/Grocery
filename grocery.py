852f2f7fdf0e004978f66f82b804c42976b91470
Basem Atiye <basematiye1@gmail.com>
2025-09-30 15:40:49 +0300

    full code

--- grocery.py
+++ grocery.py
@@ -8,27 +8,46 @@ groceries = {
     "mangoes": 8,
     "kiwis": 4,
     "strawberries": 10,
-    "blueberries": 9
+    "blueberries": 9,
+    "milk": 6
 }
 
-n=len(groceries)
-for i in range (n):
-    item=list(groceries.keys())[i]
-    price=groceries[item]
+for item, price in groceries.items():
     print(f"{item}: ${price}")
 
-cart={}
-user_input = ""
+cart = {}
 total_price = 0
-while user_input != "done":
-    user_input = input("Enter an item to add to your cart (or 'done' to exit): ")
+user_input = ""
+
+while True:
+    user_input = input("Enter an item and quantity (e.g. 'apple 3') or 'done' to exit: ").strip()
+    
     if user_input == "done":
-        print("Thank you for shopping with us!")
+        print("\nThank you for shopping with us!")
         print("Your cart:", cart)
-        print("Total price: $", total_price)
+
+        final_price = 0
+        for item, quantity in cart.items():
+            price = groceries[item]
+            if item == "milk" and quantity > 2:
+                price = price-1  
+            final_price += price * quantity
+        
+        print("Total price: $", final_price)
         break
-    elif user_input in groceries:
-        cart[user_input] = groceries[user_input]
-        total_price += groceries[user_input]
+
+    parts = user_input.split()
+    if len(parts) != 2:
+        print("Please enter in the format 'item quantity'.")
+        continue
+
+    item = parts[0]
+    quantity_str = parts[1]
+    if item not in groceries:
+        print(f"Sorry, {item} is not available in the grocery list.")
+        continue
+
+    if item in cart:
+        cart[item] += int(quantity_str)
     else:
-        print(f"Sorry, {user_input} is not available in the grocery list.")
+        cart[item] = int(quantity_str)
